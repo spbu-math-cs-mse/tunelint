a22e34330f64d17871790ac9c3347f5380652a20
Sasha239 <alex.bogdanov05@mail.ru>
2024-11-21 17:06:16 +0300

    Move test

--- composeApp/src/commonTest/kotlin/org/goalteam/tunelint/filesupport/XMLParserTest.kt
+++ composeApp/src/commonTest/kotlin/org/goalteam/tunelint/filesupport/XMLParserTest.kt
@@ -1,9 +1,9 @@
 @file:Suppress("ktlint:standard:filename")
 
-package filesupport
+package org.goalteam.tunelint.filesupport
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class XMLParserTest {
     @Test
