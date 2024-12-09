a9100152e0f80c9ba16c74481f32f3e1bd61a516
Sasha239 <alex.bogdanov05@mail.ru>
2024-12-09 21:42:19 +0300

    Implement AddMeasure button + appearance changes

--- composeApp/src/commonMain/kotlin/org/goalteam/tunelint/view/toolbar/ModeButtons.kt
+++ composeApp/src/commonMain/kotlin/org/goalteam/tunelint/view/toolbar/ModeButtons.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.goalteam.tunelint.interaction.events.Mode
--- composeApp/src/commonMain/kotlin/org/goalteam/tunelint/view/toolbar/NoteIcon.kt
+++ composeApp/src/commonMain/kotlin/org/goalteam/tunelint/view/toolbar/NoteIcon.kt
@@ -1,10 +1,16 @@
 package org.goalteam.tunelint.view.toolbar
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.goalteam.tunelint.model.core.MusicFactory
 import org.goalteam.tunelint.model.core.NoteOffset
 import org.goalteam.tunelint.model.core.PrimaryNoteValue
+import org.goalteam.tunelint.view.musicsheet.GeometryData
 import org.goalteam.tunelint.view.musicsheet.composable.UnleveredNoteView
+import org.goalteam.tunelint.view.musicsheet.viewable.horizontalSteps
 import org.goalteam.tunelint.view.musicsheet.viewable.impl.NoteViewableImpl
 
 @Composable
