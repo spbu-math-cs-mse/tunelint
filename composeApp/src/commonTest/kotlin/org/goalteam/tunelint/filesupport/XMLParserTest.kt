17a6ea2f3e1265cff2bd022b241a439279035eaa
1mpact100 <sivanov090909@gmail.com>
2024-12-11 04:22:24 +0300

    Add tests for XMLParser

--- composeApp/src/commonTest/kotlin/org/goalteam/tunelint/filesupport/XMLParserTest.kt
+++ composeApp/src/commonTest/kotlin/org/goalteam/tunelint/filesupport/XMLParserTest.kt
@@ -21,4 +21,25 @@ class XMLParserTest {
         assertEquals(melodyList.size, 2)
         parser.writeMusic("././././././music/parser_test_output_2.xml", melodyList)
     }
+
+    @Test
+    fun test_3() {
+        val melodyList = parser.readMusic("././././././music/parser_test_input_3.xml")
+        assertEquals(melodyList.size, 1)
+        parser.writeMusic("././././././music/parser_test_output_3.xml", melodyList)
+    }
+
+    @Test
+    fun test_4() {
+        val melodyList = parser.readMusic("././././././music/parser_test_input_4.xml")
+        assertEquals(melodyList.size, 0)
+        parser.writeMusic("././././././music/parser_test_output_4.xml", melodyList)
+    }
+
+    @Test
+    fun test_5() {
+        val melodyList = parser.readMusic("././././././music/parser_test_input_5.xml")
+        assertEquals(melodyList.size, 3)
+        parser.writeMusic("././././././music/parser_test_output_5.xml", melodyList)
+    }
 }
