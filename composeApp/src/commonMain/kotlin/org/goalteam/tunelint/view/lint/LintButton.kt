f3be5a61283ebbe2c43c609cfe9d515168ab6cbd
Vasily Fedorov <vasily.fedorov@arsysop.ru>
2024-12-22 11:44:39 +0300

    #49 lint view

--- composeApp/src/commonMain/kotlin/org/goalteam/tunelint/view/lint/LintButton.kt
+++ composeApp/src/commonMain/kotlin/org/goalteam/tunelint/view/lint/LintButton.kt
@@ -1,6 +1,7 @@
 package org.goalteam.tunelint.view.lint
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
